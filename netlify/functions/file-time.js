43a17fb90aefb79d1527961ed65e7cda4858946a
台灣智能產品開發-林聖惇(Mars Lin) <mars.lin@meiloon.com>
2025-02-27 08:46:39 +0800

    Update file-time.js

--- netlify/functions/file-time.js
+++ netlify/functions/file-time.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
 // const response = await fetch("https://ml-webservice.netlify.app/udid/Apple_mobile_device_types.txt");
 // const filePath = path.join(process.cwd(), 'udid/Apple_mobile_device_types.txt');
 //     const filePath = path.join(__dirname, '../../public/file/mlcontrol/mlcontrol.plist');  // 檔案的實際路徑
-    const filePath = path.join(process.cwd(), 'udid/Apple_mobile_device_types.txt');
+    const filePath = path.join(process.cwd(), 'file/mlcontrol/app-release.apk');
 
     try {
         const stats = fs.statSync(filePath);  // 同步讀取檔案的統計信息
