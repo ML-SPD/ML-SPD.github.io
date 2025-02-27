1aeebf85ebc4c2de1d3e3d58cd36c4981c4e2675
台灣智能產品開發-林聖惇(Mars Lin) <mars.lin@meiloon.com>
2025-02-27 08:29:40 +0800

    Add netlify function
    Add file-time function

---
+++ netlify/functions/file-time.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+// const response = await fetch("https://ml-webservice.netlify.app/udid/Apple_mobile_device_types.txt");
+// const filePath = path.join(process.cwd(), 'udid/Apple_mobile_device_types.txt');
+// /Users/A13017/Documents/GitHub/ML-SPD.github.io/file/mlcontrol/app-release.apk
+    const filePath = path.join(process.cwd(), 'file/mlcontrol/app-release.apk');  // 檔案的實際路徑
+
+    try {
+        const stats = fs.statSync(filePath);  // 同步讀取檔案的統計信息
+        const lastModified = stats.mtime.toISOString();  // 取得修改時間並格式化為 ISO 字符串
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ lastModified }),  // 返回 JSON 格式的時間
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error reading file' }),
+        };
+    }
+};
